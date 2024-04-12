acb322b80fb37a4b93a0903dd10e63b1f48b0f84
PKLogger <senseicuong@gmail.com>
2024-04-12 17:23:37 +0700

    add scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Providers } from './providers';
 import { fonts } from './fonts'
 import "./globals.css";
+import "../styles/index.scss";
 
 export const metadata: Metadata = {
   title: "Create Next App",
